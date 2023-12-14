70f38567a89422c9825eb797fc84fa8fe7206db1
hlarda <halareda63@gmail.com>
2023-12-14 11:21:15 +0200

    relay app

---
+++ HAL/relay/relayApp.c
@@ -0,0 +1,27 @@
+#include"DIO/DIO.h"
+#include<util/delay.h>
+
+int main()
+{
+  DIOsetPinDirection(PC0,OUTPUT_PIN);
+  DIOsetPinDirection(PC1,OUTPUT_PIN);
+
+  
+  while (1)
+  {
+    DIOsetPinLogic(PC0,HIGH_PIN);
+    DIOsetPinLogic(PC1,LOW_PIN);
+    _delay_ms(500);
+    DIOsetPinLogic(PC0,LOW_PIN);
+    DIOsetPinLogic(PC1,LOW_PIN);
+    _delay_ms(500);
+    DIOsetPinLogic(PC0,LOW_PIN);
+    DIOsetPinLogic(PC1,HIGH_PIN);
+    _delay_ms(500);
+    DIOsetPinLogic(PC0,LOW_PIN);
+    DIOsetPinLogic(PC1,LOW_PIN);
+    _delay_ms(500);
+    
+  }
+  return 0;
+}
