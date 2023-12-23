5392e19aa748be0b27545f06a7636373c9d0e83a
hlarda <halareda63@gmail.com>
2023-12-23 15:14:31 +0200

    works when{1async,1 or more sync} at a time

--- MCAL/ADC/ADCapp.c
+++ MCAL/ADC/ADCapp.c
@@ -1,5 +1,6 @@
 #include <DIO/DIO.h>
-#include <ADC/ADC.h>   
+#include <ADC/ADC.h> 
+#include <GINT/GINT.h>  
 #include <util/delay.h>
 
 u8 map(u32 INmax, u32 INmin, u32 OUTmax, u32 OUTmin, u32 IN);
@@ -7,17 +8,36 @@ int main() {
     DIOsetPortDirection(portC,OUTPUT_REG);
     DIOsetPortLogic(portC,HIGH_REG);
 
-    DIOsetPinDirection(PA0,INPUT_PIN);
-    ADCx_t selectedChannel = ADC0;
-    ADCinit();
+    DIOsetPortDirection(portD,OUTPUT_REG);
+    DIOsetPortLogic(portD,HIGH_REG);
 
-    while (1)
-    {
-        u16 adcData=ADCread(selectedChannel);
-        u8 mappedValue = map(1023,0,255,0,adcData);
-        DIOsetPortLogic(portC,mappedValue);
-        _delay_ms(10);
-    }
+    DIOsetPortDirection(portB,OUTPUT_REG);
+    DIOsetPortLogic(portB,HIGH_REG);
+
+    DIOsetPinDirection(PA0,INPUT_PIN);
+    DIOsetPinDirection(PA3,INPUT_PIN);
+    DIOsetPinDirection(PA7,INPUT_PIN);
+    ADCinit();
+    globalInterruptEnable();
+
+    while (1){
+        
+        u16 adc0Data=ADCread(ADC0);
+        u8 mappedValueSync0 = map(1023,0,255,0,adc0Data);
+        DIOsetPortLogic(portC,mappedValueSync0);
+
+        u16 adc3Data=ADCread(ADC3);
+        u8 mappedValueSync3 = map(1023,0,255,0,adc3Data);
+        DIOsetPortLogic(portB,mappedValueSync3);
+
+        //ADCasyncStart(ADC3);
+        //u8 mappedValueSync3 = map(1023,0,255,0,ADC_ISR_value);
+        //DIOsetPortLogic(portD,mappedValueSync3);
+
+        //ADCasyncStart(ADC7);
+        //u8 mappedValueASync7 = map(1023,0,255,0,ADC_ISR_value);
+        //DIOsetPortLogic(portD,mappedValueASync7);
+    }   
     return 0;
 }
 u8 map(u32 INmax, u32 INmin, u32 OUTmax, u32 OUTmin, u32 IN) {
