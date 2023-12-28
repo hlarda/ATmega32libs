5e72c957d57b228601eeab791b8ce86d2653af9b
hlarda <halareda63@gmail.com>
2023-12-29 00:53:33 +0200

    tested on board

--- MCAL/DIO/DIOapp.c
+++ MCAL/DIO/DIOapp.c
@@ -1,17 +1,20 @@
 #include "DIO/DIO.h"
 
 int main(void) {
-  DIOsetPinDirection(PA0, OUTPUT_PIN);
-  DIOsetPinDirection(PB0, OUTPUT_PIN);
-  DIOsetPinLogic(PA0, HIGH_PIN);
-  pinLogic_t pinLogic = DIOgetPinLogic(PA0);
-  DIOsetPinLogic(PB0,pinLogic);
+    //DIOsetPinDirection(PA0, OUTPUT_PIN);
+    //DIOsetPinDirection(PA5, OUTPUT_PIN);
 
-  DIOsetPortDirection(portC, OUTPUT_REG);
-  DIOsetPortDirection(portD, OUTPUT_REG);
-  DIOsetPortLogic(portC, HIGH_REG);
-  registerLogic_t portClogic = DIOgetPortLogic(portC);
-  DIOsetPortLogic(portD,portClogic);
+    DIOsetPortDirection(portA, OUTPUT_REG);
+    DIOsetPortDirection(portB, OUTPUT_REG);
+  while (1)
+  {
+    //DIOsetPinLogic(PA0, HIGH_PIN);
+    //pinLogic_t pinLogic = DIOgetPinLogic(PA0);
+    //DIOsetPinLogic(PA5,pinLogic);
 
+    DIOsetPortLogic(portB, HIGH_REG);
+    registerLogic_t portClogic = DIOgetPortLogic(portB);
+    DIOsetPortLogic(portA,portClogic);
+  }
   return 0;
 }
