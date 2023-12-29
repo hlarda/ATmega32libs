14aeb26f1bca9f94f9a74d5ebb14665b6a972a7a
hlarda <halareda63@gmail.com>
2023-12-29 19:00:48 +0200

    reversed pins order for wiring matter

--- HAL/LCD/LCDconfig.h
+++ HAL/LCD/LCDconfig.h
@@ -1,18 +1,18 @@
 #ifndef LCD_CONFIG_H_
 #define LCD_CONFIG_H_
 
-#define LCD_PD0                PA0
-#define LCD_PD1                PA1
-#define LCD_PD2                PA2
-#define LCD_PD3                PA3
-#define LCD_PD4                PA4
-#define LCD_PD5                PA5
-#define LCD_PD6                PA6
-#define LCD_PD7                PA7
+#define LCD_PD0                PA7
+#define LCD_PD1                PA6
+#define LCD_PD2                PA5
+#define LCD_PD3                PA4
+#define LCD_PD4                PA3
+#define LCD_PD5                PA2
+#define LCD_PD6                PA1
+#define LCD_PD7                PA0
 
-#define LCD_RS                 PB1
-#define LCD_RW                 PB2
-#define LCD_EN                 PB3
+#define LCD_RS                 PB7
+#define LCD_RW                 PB6
+#define LCD_EN                 PB5
 
 /*
  * LCD_MODE OPTIONS :  [ _4_BIT_MODE , _8_BIT_MODE ]
@@ -20,3 +20,4 @@
 #define LCD_MODE          _8_BIT_MODE
 
 #endif 
+
